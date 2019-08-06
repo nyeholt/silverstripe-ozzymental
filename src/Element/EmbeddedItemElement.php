eeaa21cb5eb83668348d9b8a18ba9204953df108
Marcus Nyeholt <marcus@symbiote.com.au>
2019-08-06 20:10:23 +1000

    chore(EmbeddedItem) Added getType method

--- src/Element/EmbeddedItemElement.php
+++ src/Element/EmbeddedItemElement.php
@@ -13,29 +13,29 @@ use DNADesign\Elemental\Models\BaseElement;
  *
  * @author marcus
  */
-class EmbeddedItemElement extends BaseElement 
+class EmbeddedItemElement extends BaseElement
 {
     private static $title = 'Embedded item';
 
     private static $table_name = 'EmbeddedItemElement';
-    
+
     private static $description = 'An embeddable item, such as video, image, or other website';
-    
+
     private static $has_one = array(
         'EmbeddedItem'      => EmbeddedObject::class,
     );
-    
-    
+
+
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
         $fields->removeByName('EmbeddedItemID');
         $i = $this->EmbeddedItem();
         $fields->addFieldToTab('Root.Main', EmbeddedObjectField::create('EmbeddedItem', 'Item URL', $this->EmbeddedItem()));
-        
+
         return $fields;
     }
-    
+
     public function onBeforeWrite()
     {
         parent::onBeforeWrite();
@@ -43,5 +43,10 @@ class EmbeddedItemElement extends BaseElement
         if ($item && $item->ID && strlen($item->Title)) {
             $this->Title = $item->Title;
         }
+    }
+
+    public function getType()
+    {
+        return _t(__CLASS__ . '.BlockType', 'Embedded Item');
     }
 }
